2d79367b0e49e72e7d8eefcaa9cab8d2f90b6fa7
maycondev01 <mayconlundgren186@gmail.com>
2024-01-30 10:45:00 -0300

    Initial Commit

---
+++ src/test/java/br/com/maycondouglas/desafiotodolist/DesafioTodoListApplication.java
@@ -0,0 +1,14 @@
+package br.com.maycondouglas.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class DesafioTodoListApplication {
+    
+    @Test
+    void contextLoads() {
+        
+    }
+}
